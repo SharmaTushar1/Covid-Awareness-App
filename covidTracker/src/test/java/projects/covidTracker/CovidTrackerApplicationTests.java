a6b4fa58e74dc01c12e77ca0d96477245dcf7936
Tushar Sharma <tusharsharma2440160@gmail.com>
2022-07-01 17:22:21 +0530

    Add files

---
+++ covidTracker/src/test/java/projects/covidTracker/CovidTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.covidTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
